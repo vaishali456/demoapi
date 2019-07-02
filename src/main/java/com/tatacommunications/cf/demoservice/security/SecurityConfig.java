f6b3f42544bffbf2858f34730d2362f31f934edd
vaishali456 <vaishubhaip@gmail.com>
2019-07-02 20:18:41 +0530

    http.cors();

--- src/main/java/com/tatacommunications/cf/demoservice/security/SecurityConfig.java
+++ src/main/java/com/tatacommunications/cf/demoservice/security/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/pocapp/**").hasRole("USER")
 				.antMatchers("/**").hasRole("ADMIN")
 				.and().csrf().disable().headers().frameOptions().disable();
+		 http.cors();
 	}
 
 }
